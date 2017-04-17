12b98686eb1fd16bb667f10305fdb0cf4f8fc967
Diomidis Spinellis <dds@aueb.gr>
2017-04-17 18:27:31 +0300

    More robust argument processing

--- make-tags.sh
+++ make-tags.sh
@@ -18,8 +18,8 @@
 # limitations under the License.
 #
 
-for i in $* ; do
+for i in "$@" ; do
   sed -nr \
-    "s|^.*(create  *table  *([^.]*\\.)?([^ (]*)).*|\\3\t$i\t/\1/|ip" $i
+    "s|^.*(create  *table  *([^.]*\\.)?([^ (]*)).*|\\3\t$i\t/\1/|ip" "$i"
 done |
 sort >tags

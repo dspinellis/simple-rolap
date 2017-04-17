98dcd98a51f79ae3aa01892d3a42cb0e2e4e4d8e
Diomidis Spinellis <dds@aueb.gr>
2017-04-17 18:32:58 +0300

    Prevent field separation errors

--- mkdep.sh
+++ mkdep.sh
@@ -19,12 +19,12 @@
 #
 
 for i in *.sql ; do
-  base=$(basename $i .sql)
-  if grep '^select' $i >/dev/null ; then
+  base=$(basename "$i" .sql)
+  if grep '^select' "$i" >/dev/null ; then
     target="reports\\/$base.txt"
   else
     target="tables\\/$base"
   fi
-  sed -rn "/^delete/Q;s/^.*(from|join)  *$MAINDB\.([a-zA-Z][-_a-zA-Z0-9]*).*\$/$target: tables\/\2/p" $i
+  sed -rn "/^delete/Q;s/^.*(from|join)  *$MAINDB\.([a-zA-Z][-_a-zA-Z0-9]*).*\$/$target: tables\/\2/p" "$i"
 done |
 sort -u

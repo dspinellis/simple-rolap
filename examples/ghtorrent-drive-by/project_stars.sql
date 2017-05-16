51c768f030e22020d1ba91f97d3be7dab47cfdcc
Diomidis Spinellis <dds@aueb.gr>
2017-05-17 00:03:12 +0300

    Fix counting of stars

--- examples/ghtorrent-drive-by/project_stars.sql
+++ examples/ghtorrent-drive-by/project_stars.sql
@@ -1,7 +1,7 @@
 -- Projects with recent issues and the number of stars
 
 create table driveby.project_stars AS
-  select recent_issue_projects.id as id, count(*) as stars
+  select recent_issue_projects.id as id, count(watchers.repo_id) as stars
   from driveby.recent_issue_projects
   left join watchers
   on watchers.repo_id = recent_issue_projects.id

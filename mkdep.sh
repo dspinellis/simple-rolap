e2a3888a9e49d00fc617de5aedc57063e1a7fe13
Diomidis Spinellis <dds@aueb.gr>
2019-01-30 10:15:00 +0200

    Ignore case in SQL matching

--- mkdep.sh
+++ mkdep.sh
@@ -20,11 +20,11 @@
 
 for i in *.sql ; do
   base=$(basename "$i" .sql)
-  if grep '^select' "$i" >/dev/null ; then
+  if grep -i '^select' "$i" >/dev/null ; then
     target="reports\\/$base.txt"
   else
     target="tables\\/$base"
   fi
-  sed -rn "/^delete/Q;s/^.*(from|join)  *$ROLAPDB\.([a-zA-Z][-_a-zA-Z0-9]*).*\$/$target: tables\/\2/p" "$i"
+  sed -rn "/^delete/iQ;s/^.*(from|join)  *$ROLAPDB\.([a-zA-Z][-_a-zA-Z0-9]*).*\$/$target: tables\/\2/ip" "$i"
 done |
 sort -u
